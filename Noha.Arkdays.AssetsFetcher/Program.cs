4c3391c10ee13b180422b35f8517501492c6d3cb
Yuze Fu <i@xfox.me>
2020-04-22 16:38:14 +0800

    feat(fetcher): add assets fetcher

---
+++ Noha.Arkdays.AssetsFetcher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Noha.Arkdays.AssetsFetcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
